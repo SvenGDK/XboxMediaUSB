d87faf5f28a58f54b486d8ae09e5ca9041ca894e
SvenGDK <svengdk@hotmail.com>
2022-10-16 16:36:04 +0200

    First release

---
+++ XboxMediaUSB/MainForm.Designer.vb
@@ -0,0 +1,173 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.StatusLabel = New System.Windows.Forms.Label
+        Me.StatusProgressBar = New System.Windows.Forms.ProgressBar
+        Me.StartButton = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.DriveList1 = New System.Windows.Forms.ComboBox
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.SetPermissionsButton = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.DriveList2 = New System.Windows.Forms.ComboBox
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.StatusLabel)
+        Me.GroupBox1.Controls.Add(Me.StatusProgressBar)
+        Me.GroupBox1.Controls.Add(Me.StartButton)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.DriveList1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(616, 215)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Prepare new a USB drive"
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Location = New System.Drawing.Point(17, 152)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(500, 20)
+        Me.StatusLabel.TabIndex = 8
+        Me.StatusLabel.Text = "%STATUS%"
+        Me.StatusLabel.Visible = False
+        '
+        'StatusProgressBar
+        '
+        Me.StatusProgressBar.Location = New System.Drawing.Point(17, 175)
+        Me.StatusProgressBar.Name = "StatusProgressBar"
+        Me.StatusProgressBar.Size = New System.Drawing.Size(500, 23)
+        Me.StatusProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee
+        Me.StatusProgressBar.TabIndex = 7
+        Me.StatusProgressBar.Visible = False
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(523, 66)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(87, 30)
+        Me.StartButton.TabIndex = 6
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(17, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(500, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Warning: All Data on this drive will be erased."
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(504, 23)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Select removable drive:"
+        '
+        'DriveList1
+        '
+        Me.DriveList1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.DriveList1.FormattingEnabled = True
+        Me.DriveList1.Location = New System.Drawing.Point(17, 66)
+        Me.DriveList1.Name = "DriveList1"
+        Me.DriveList1.Size = New System.Drawing.Size(500, 28)
+        Me.DriveList1.TabIndex = 2
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.SetPermissionsButton)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.DriveList2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 233)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(616, 144)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Set only permissions on drive (files will not be deleted)"
+        '
+        'SetPermissionsButton
+        '
+        Me.SetPermissionsButton.Location = New System.Drawing.Point(465, 98)
+        Me.SetPermissionsButton.Name = "SetPermissionsButton"
+        Me.SetPermissionsButton.Size = New System.Drawing.Size(145, 30)
+        Me.SetPermissionsButton.TabIndex = 9
+        Me.SetPermissionsButton.Text = "Set permissions"
+        Me.SetPermissionsButton.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(10, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(504, 23)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Select removable drive:"
+        '
+        'DriveList2
+        '
+        Me.DriveList2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.DriveList2.FormattingEnabled = True
+        Me.DriveList2.Location = New System.Drawing.Point(14, 64)
+        Me.DriveList2.Name = "DriveList2"
+        Me.DriveList2.Size = New System.Drawing.Size(596, 28)
+        Me.DriveList2.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(645, 391)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "XboxMediaUSB v1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents DriveList1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents StartButton As System.Windows.Forms.Button
+    Friend WithEvents StatusProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents StatusLabel As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents SetPermissionsButton As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents DriveList2 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ XboxMediaUSB/MainForm.vb
@@ -0,0 +1,168 @@
+Imports System.IO
+
+Public Class MainForm
+
+    Dim SelectedDrive As IO.DriveInfo
+
+    Dim WithEvents FormatWorker As New System.ComponentModel.BackgroundWorker()
+    Dim WithEvents PermissionWorker As New System.ComponentModel.BackgroundWorker()
+
+    Delegate Sub UpdateTextStatusDelegate(ByVal statLabel As Label, ByVal stringValue As String)
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        For Each Drive As DriveInfo In DriveInfo.GetDrives()
+            If Drive.DriveType = DriveType.Removable And Drive.IsReady Or DriveType.Fixed And Drive.IsReady Then
+                DriveList1.Items.Add(Drive.Name + vbTab + Drive.VolumeLabel)
+                DriveList2.Items.Add(Drive.Name + vbTab + Drive.VolumeLabel)
+            End If
+        Next
+    End Sub
+
+    Public Sub TextDelegateMethod(ByVal statLabel As Label, ByVal stringValue As String)
+        statLabel.Text = stringValue
+    End Sub
+
+    Public Sub FormatDrive()
+        Dim FormatStartInfo As New ProcessStartInfo()
+        FormatStartInfo.FileName = "format.com"
+        FormatStartInfo.Arguments = "/fs:NTFS /v:XboxMediaUSB /q " + SelectedDrive.Name.Remove(2)
+        FormatStartInfo.UseShellExecute = False
+        FormatStartInfo.CreateNoWindow = True
+        FormatStartInfo.RedirectStandardOutput = True
+        FormatStartInfo.RedirectStandardInput = True
+
+        Dim FormatProcess As Process = Process.Start(FormatStartInfo)
+
+        Dim processInputStream As IO.StreamWriter = FormatProcess.StandardInput
+        processInputStream.Write(vbCr & vbLf)
+
+        FormatProcess.WaitForExit()
+    End Sub
+
+    Private Sub ComboBox1_SelectedValueChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles DriveList1.SelectedValueChanged
+        If DriveList1.SelectedItem IsNot Nothing Then
+            Try
+                SelectedDrive = New DriveInfo(DriveList1.Text.Split(vbTab)(0))
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub DriveList2_SelectedValueChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles DriveList2.SelectedValueChanged
+        If DriveList2.SelectedItem IsNot Nothing Then
+            Try
+                SelectedDrive = New DriveInfo(DriveList2.Text.Split(vbTab)(0))
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub FormatWorker_DoWork(ByVal sender As Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles FormatWorker.DoWork
+        Try
+            FormatDrive()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub FormatWorker_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles FormatWorker.RunWorkerCompleted
+        Me.Cursor = Cursors.Default
+
+        If StatusLabel.InvokeRequired Then
+            StatusLabel.BeginInvoke(New UpdateTextStatusDelegate(AddressOf TextDelegateMethod), SelectedDrive.Name + " formated!")
+        Else
+            StatusLabel.Text = SelectedDrive.Name + " formated!"
+        End If
+
+        StatusProgressBar.Visible = False
+
+        If MsgBox("Do you want to create the 'Games' and 'RetroArch' folders on your new drive?", MsgBoxStyle.YesNo, "Directories") = MsgBoxResult.Yes Then
+            Directory.CreateDirectory(SelectedDrive.Name + "Games")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Atari")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Atari\2600")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Atari\Jaguar")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\DS")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\GameBoy")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\GameBoyColor")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\GameBoyAdvance")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\GameCube")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\N64")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\NES")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\SuperNintendo")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Nintendo\Wii")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Playstation")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Playstation\PS1")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Playstation\PS2")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Playstation\PSP")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Sega")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Sega\Dreamcast")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Sega\GameGear")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Sega\Genesis-MegaDrive")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Sega\MasterSystem")
+            Directory.CreateDirectory(SelectedDrive.Name + "Games\Sega\Saturn")
+
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\assets")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\cheats")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\configs")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\info")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\logs")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\overlays")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\playlists")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\saves")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\shaders")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\states")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\system")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\system\dolphin-emu")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\system\pcsx2")
+            Directory.CreateDirectory(SelectedDrive.Name + "RetroArch\thumbnails")
+        End If
+
+        If StatusLabel.InvokeRequired Then
+            StatusLabel.BeginInvoke(New UpdateTextStatusDelegate(AddressOf TextDelegateMethod), "Setting permissions on " + SelectedDrive.Name)
+        Else
+            StatusLabel.Text = "Setting permissions on " + SelectedDrive.Name
+        End If
+
+        PermissionWorker.RunWorkerAsync()
+    End Sub
+
+    Private Sub PermissionWorker_DoWork(ByVal sender As Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles PermissionWorker.DoWork
+        Try
+            Dim AllAppPKGUser As New Security.Principal.SecurityIdentifier("S-1-15-2-1") 'ALL APPLICATION PACKAGES
+            Dim SecRule As New Security.AccessControl.FileSystemAccessRule(AllAppPKGUser, Security.AccessControl.FileSystemRights.FullControl, Security.AccessControl.InheritanceFlags.ContainerInherit Or Security.AccessControl.InheritanceFlags.ObjectInherit, Security.AccessControl.PropagationFlags.None, Security.AccessControl.AccessControlType.Allow)
+            Dim CurrentDirPermissions As Security.AccessControl.DirectorySecurity = Directory.GetAccessControl(SelectedDrive.Name)
+
+            CurrentDirPermissions.AddAccessRule(SecRule)
+            Directory.SetAccessControl(SelectedDrive.Name, CurrentDirPermissions)
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub PermissionWorker_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles PermissionWorker.RunWorkerCompleted
+        MsgBox(SelectedDrive.Name + " is now prepared to use with your Xbox.", MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles StartButton.Click
+        If MsgBox("Do you really want to format " + SelectedDrive.Name + " ? All exisisting data on this drive will be erased.", MsgBoxStyle.YesNo, "Please confirm") = MsgBoxResult.Yes Then
+            StatusLabel.Visible = True
+            StatusProgressBar.Visible = True
+            StatusLabel.Text = "Formatting drive " + SelectedDrive.Name
+            Me.Cursor = Cursors.WaitCursor
+            FormatWorker.RunWorkerAsync()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SetPermissionsButton.Click
+        If DriveList2.SelectedItem IsNot Nothing Then
+            PermissionWorker.RunWorkerAsync()
+        Else
+            MsgBox("Please select a drive first.")
+        End If
+    End Sub
+
+End Class
